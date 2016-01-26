24373a7305795b9bd0609eb9694cbdd9fe8b2ef3
StemDev <stembolt.developer@gmail.com>
2016-01-25 22:07:41 -0800

    added RefreshScreen.class with onCreate() PowerManager to acquire wake lock

--- app/src/main/java/com/stembo/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/stembo/android/criminalintent/CrimeListActivity.java
@@ -1,8 +1,12 @@
 package com.stembo.android.criminalintent;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 import java.util.UUID;
 
@@ -13,6 +17,7 @@ public class CrimeListActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment(){
+
         return new CrimeListFragment();
     }
 
--- app/src/main/java/com/stembo/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/stembo/android/criminalintent/SingleFragmentActivity.java
@@ -1,14 +1,20 @@
 package com.stembo.android.criminalintent;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.view.WindowManager;
 
 /**
  * Created by myxom on 16/01/15.
  */
 public abstract class SingleFragmentActivity extends FragmentActivity {
+
+    int max_bright = 255;
 
     protected abstract Fragment createFragment();
 
@@ -16,6 +22,9 @@ public abstract class SingleFragmentActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+
+        Intent bright_intent = new Intent(SingleFragmentActivity.this, RefreshScreen.class);
+        startActivity(bright_intent);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
